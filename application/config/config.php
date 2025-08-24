1cbbef6d61c65b0374895952363cac400bf587ce
Supranatha <ketutsupranatha@gmail.com>
2025-08-24 09:12:32 +0800

    update

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/web-desa-ci3/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
